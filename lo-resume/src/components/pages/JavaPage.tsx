c8447a9ed493140ed1e7afe7d80350a415fbfbad
LukasOscipovsky <lukas.oscipovsky@gmail.com>
2020-05-03 09:20:09 +0200

    second page related java

--- lo-resume/src/components/pages/JavaPage.tsx
+++ lo-resume/src/components/pages/JavaPage.tsx
@@ -8,7 +8,25 @@ interface Props {
     triggerRender(id: number): void
 }
 
-export default class JavaPage extends React.Component<Props, {}> {
+interface State {
+    scaleUpWidth: string;
+    scaleDownWidth: string;
+    scaleUpHeight: string;
+    scaleDownHeight: string;
+}
+
+export default class JavaPage extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            scaleUpWidth: '5vw',
+            scaleUpHeight: '25%',
+            scaleDownWidth: '50vw',
+            scaleDownHeight: '90%'
+        }
+    }
+
+
     trigger() {
         let id = this.props.id;
         this.props.triggerRender(id++);
@@ -16,10 +34,38 @@ export default class JavaPage extends React.Component<Props, {}> {
 
     render() {
         return (
-            <div className="app">
-                <div className="info">
-                    <label className="name">{this.props.name}</label>
-                    <label className="role">{this.props.role.join(",")}</label>
+            <div className="jpl">
+                <div className="techs">
+                    <div className="tech" style={{ width: '25vw', height: '90%' }}>
+                        <img className="img"
+                            src={require("../../assets/Java-Logo-Wallpaper.jpg")}
+                            alt="javalogo" />
+                        <label className="label">Java</label>
+                    </div>
+                    <div className="tech" style={{ width: '5vw', height: '25%' }}>
+                        <img className="img"
+                            src={require("../../assets/OG-Spring.png")}
+                            alt="springlogo" />
+                        <label className="label">Spring</label>
+                    </div>
+                    <div className="tech" style={{ width: '17vw', height: '75%' }}>
+                        <img className="img"
+                            src={require("../../assets/hibernate.jpg")}
+                            alt="hiblogo" />
+                        <label className="label">Hibernate</label>
+                    </div>
+                    <div className="tech" style={{ width: '17vw', height: '75%' }}>
+                        <img className="img"
+                            src={require("../../assets/springdata.png")}
+                            alt="sdlogo" />
+                        <label className="label">Spring Data</label>
+                    </div>
+                    <div className="tech" style={{ width: '17vw', height: '75%' }}>
+                        <img className="img"
+                            src={require("../../assets/springboot.jpg")}
+                            alt="sblogo" />
+                        <label className="label">Spring Boot</label>
+                    </div>
                 </div>
                 <Arrow triggerClicked={() => this.trigger()} />
             </div>
